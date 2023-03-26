8ecb400f7578b04e5a86a5d30f8c4602d1de9d35
Lawrence Kesteloot <lk@teamten.com>
2023-03-26 16:53:16 -0700

    Draw shape from letter.

--- ts/test.ts
+++ ts/test.ts
@@ -1,4 +1,6 @@
 
+import opentype from  "https://unpkg.com/opentype.js@1.3.4/dist/opentype.module.js";
+
 const DPI = 72;
 const CURVE_TIGHTNESS = 0; // 0 = Catmull-Rom splines, 1 = straight lines
 
@@ -32,12 +34,44 @@ export class Vector {
         }
     }
 
-    public dividedBy(s: number): Vector {
-        return new Vector(this.x/s, this.y/s);
+    public dividedBy(s: number | Vector): Vector {
+        if (s instanceof Vector) {
+            return new Vector(this.x/s.x, this.y/s.y);
+        } else {
+            return new Vector(this.x/s, this.y/s);
+        }
+    }
+
+    public negate(): Vector {
+        return new Vector(-this.x, -this.y);
     }
 
     public lengthSquared(): number {
-        return this.x*this.x + this.y*this.y;
+        return this.dot(this);
+    }
+
+    public length(): number {
+        return Math.sqrt(this.lengthSquared());
+    }
+
+    public lerp(p: Vector, t: number): Vector {
+        return this.plus(p.minus(this).times(t));
+    }
+
+    public perpendicular(): Vector {
+        return new Vector(this.y, -this.x);
+    }
+
+    public normalized(): Vector {
+        const length = this.length();
+        if (length === 0) {
+            throw new Error("can't normalize zero-length vector");
+        }
+        return this.dividedBy(length);
+    }
+
+    public dot(p: Vector): number {
+        return this.x*p.x + this.y*p.y;
     }
 }
 
@@ -50,12 +84,20 @@ export class Rect {
         this.size = size;
     }
 
+    public static fromEnds(p1: Vector, p2: Vector): Rect {
+        return new Rect(p1, p2.minus(p1));
+    }
+
     public insetBy(s: number): Rect {
         return new Rect(this.p.plus(new Vector(s, s)), this.size.minus(new Vector(s*2, s*2)));
     }
 
     public randomPoint(): Vector {
         return Vector.random().times(this.size).plus(this.p);
+    }
+
+    public center(): Vector {
+        return this.p.plus(this.size.times(0.5));
     }
 }
 
@@ -72,12 +114,12 @@ export class Svg {
     }
 
     public drawLine(p1: Vector, p2: Vector) {
-        this.parts.push(`f.write("""<line x1="${p1.x}" y1="${p1.y}" x2="${p2.x}" y2="${p2.y}" stroke="blue" stroke-width="1"/>`);
+        this.parts.push(`<line x1="${p1.x}" y1="${p1.y}" x2="${p2.x}" y2="${p2.y}" stroke="blue" stroke-width="1"/>`);
     }
 
     public drawRect(p1: Vector, p2: Vector) {
         const size = p2.minus(p1);
-        this.parts.push(`f.write("""<rect x="${p1.x}" y="${p1.y}" width="${size.x}" y2="${size.y}" stroke="blue" stroke-width="1" fill="none"/>`);
+        this.parts.push(`<rect x="${p1.x}" y="${p1.y}" width="${size.x}" y2="${size.y}" stroke="blue" stroke-width="1" fill="none"/>`);
     }
 
     public drawCircle(c: Vector, r: number) {
@@ -106,6 +148,148 @@ export class Svg {
     }
 }
 
+class Line {
+    public readonly p1: Vector;
+    public readonly p2: Vector;
+
+    constructor(p1: Vector, p2: Vector) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public translateBy(p: Vector): Line {
+        return new Line(this.p1.plus(p), this.p2.plus(p));
+    }
+
+    public scaleBy(s: number | Vector): Line {
+        return new Line(this.p1.times(s), this.p2.times(s));
+    }
+
+    public lerp(t: number): Vector {
+        return this.p1.plus(this.p2.minus(this.p1).times(t));
+    }
+}
+
+// An arbitrary closed shape, ordered clockwise to include and counter-clockwise to exclude.
+class Shape {
+    public readonly lines: Line[];
+
+    constructor(lines?: Line[]) {
+        this.lines = lines ?? [];
+    }
+
+    public addBezier(p1: Vector, p2: Vector, p3: Vector, p4: Vector) {
+        const COUNT = 10;
+        const points: Vector[] = [];
+
+        for (let i = 0; i < COUNT + 1; i++) {
+            const t = i/COUNT;
+
+            const p12 = p1.lerp(p2, t);
+            const p23 = p2.lerp(p3, t);
+            const p34 = p3.lerp(p4, t);
+
+            const p123 = p12.lerp(p23, t);
+            const p234 = p23.lerp(p34, t);
+
+            const p1234 = p123.lerp(p234, t);
+            
+            points.push(p1234);
+        }
+
+        for (let i = 0; i < points.length - 1; i++) {
+            this.lines.push(new Line(points[i], points[i + 1]));
+        }
+    }
+
+    public getBbox(): Rect {
+        let x1 = this.lines[0].p1.x;
+        let y1 = this.lines[0].p1.y;
+        let x2 = this.lines[0].p1.x;
+        let y2 = this.lines[0].p1.y;
+
+        for (const line of this.lines) {
+            x1 = Math.min(x1, line.p1.x, line.p2.x);
+            y1 = Math.min(y1, line.p1.y, line.p2.y);
+            x2 = Math.max(x2, line.p1.x, line.p2.x);
+            y2 = Math.max(y2, line.p1.y, line.p2.y);
+        }
+
+        const p1 = new Vector(x1, y1);
+        const p2 = new Vector(x2, y2);
+
+        return Rect.fromEnds(p1, p2);
+    }
+
+    public translateBy(p: Vector): Shape {
+        return new Shape(this.lines.map(line => line.translateBy(p)));
+    }
+
+    public scaleBy(s: number | Vector): Shape {
+        return new Shape(this.lines.map(line => line.scaleBy(s)));
+    }
+
+    public centerIn(rect: Rect): Shape {
+        const bbox = this.getBbox();
+        const scaleBoth = rect.size.dividedBy(bbox.size);
+        const scale = Math.min(scaleBoth.x, scaleBoth.y);
+
+        return this
+            .translateBy(bbox.center().negate())
+            .scaleBy(scale)
+            .translateBy(rect.center());
+    }
+
+    public distanceToPoint(p: Vector): number {
+        let closestDistance: number | undefined = undefined;
+
+        for (const line of this.lines) {
+            // Normal vector.
+            const dl = line.p2.minus(line.p1); 
+            const n = dl.perpendicular().normalized();
+            if (n.lengthSquared() === 0) {
+                continue;
+            }
+
+            // Dot with line endpoint.
+            const dot = n.dot(p.minus(line.p1));
+
+            // Project back to line.
+            let pp = p.minus(n.times(dot));
+
+            // Find distance along line.
+            let t: number;
+            if (Math.abs(dl.x) > Math.abs(dl.y)) {
+                t = (p.x - line.p1.x)/dl.x;
+            } else {
+                t = (p.y - line.p1.y)/dl.y;
+            }
+
+            // Clamp to line.
+            t = Math.max(Math.min(t, 1), 0);
+
+            // Put back on line.
+            pp = line.lerp(t);
+
+            // Find distance.
+            let dist = p.minus(pp).length();
+
+            // Copy sign from dot product.
+            dist = dot < 0 ? -dist : dist;
+
+            if (closestDistance === undefined || Math.abs(dist) < Math.abs(closestDistance)) {
+                closestDistance = dist
+            }
+        }
+
+        if (closestDistance === undefined) {
+            throw new Error("can't find distance, no lines in shape");
+        }
+
+        return closestDistance
+    }
+}
+
 class Circle {
     public readonly c: Vector;
     public readonly r: number;
@@ -116,15 +300,26 @@ class Circle {
     }
 }
 
-function addCircle(circles: Circle[], drawArea: Rect): boolean {
+function addCircle(circles: Circle[], drawArea: Rect, shape: Shape): boolean {
     const MAX_RADIUS = 0.5*DPI;
 
     for (let attempt = 0; attempt < 5000; attempt++) {
         const p = drawArea.randomPoint();
         let minDist = MAX_RADIUS;
-        const p1 = p.minus(drawArea.p);
-        const p2 = drawArea.p.plus(drawArea.size).minus(p);
-        minDist = Math.min(minDist, p1.x, p1.y, p2.x, p2.y);
+
+        // Clip to draw area.
+        // const p1 = p.minus(drawArea.p);
+        // const p2 = drawArea.p.plus(drawArea.size).minus(p);
+        // minDist = Math.min(minDist, p1.x, p1.y, p2.x, p2.y);
+
+        // Clip to shape.
+        const dist = shape.distanceToPoint(p);
+        if (dist > 0) {
+            minDist = Math.min(minDist, dist);
+        } else {
+            // Outside of shape.
+            continue;
+        }
 
         let insideCircle = false;
         for (const c of circles) {
@@ -158,8 +353,56 @@ async function main(): Promise<void> {
 
     const drawArea = page.insetBy(margin);
 
-    while (circles.length < 5000 && true) {
-        const succeeded = addCircle(circles, drawArea);
+    const rawFontBinary = await Deno.readFile("/Library/Fonts/AGaramondPro-Regular.otf");
+    const font = opentype.parse(rawFontBinary.buffer, {});
+    const paths = font.getPaths("L");
+    const shape = new Shape();
+    let p = Vector.ZERO;
+    let firstPoint = null;
+    for (const path of paths) {
+        for (const command of path.commands) {
+            //console.log(command);
+            switch (command.type) {
+                case "M":
+                    p = new Vector(command.x, command.y);
+                    if (firstPoint === null) {
+                        firstPoint = p;
+                    }
+                    break;
+
+                case "L": {
+                    const p2 = new Vector(command.x, command.y);
+                    shape.lines.push(new Line(p, p2));
+                    p = p2;
+                    break;
+                }
+
+                case "C": {
+                    const p2 = new Vector(command.x1, command.y1);
+                    const p3 = new Vector(command.x2, command.y2);
+                    const p4 = new Vector(command.x, command.y);
+                    shape.addBezier(p, p2, p3, p4);
+                    p = p4;
+                    break;
+                }
+
+                case "Z":
+                    if (firstPoint === null) {
+                        throw new Error("cannot close path, we have no first point");
+                    }
+                    shape.lines.push(new Line(p, firstPoint));
+                    p = firstPoint;
+                    break;
+
+                default:
+                    throw new Error("unknown path command: " + command.type);
+            }
+        }
+    }
+    const bigShape = shape.centerIn(drawArea);
+
+    while (circles.length < 1000 && true) {
+        const succeeded = addCircle(circles, drawArea, bigShape);
         if (!succeeded) {
             console.log("Failed to add circle after " + circles.length + " circles");
             break;
@@ -169,6 +412,9 @@ async function main(): Promise<void> {
     const svg = new Svg(pageSize);
     for (const circle of circles) {
         svg.drawCircle(circle.c, circle.r);
+    }
+    for (const line of bigShape.lines) {
+        // svg.drawLine(line.p1, line.p2);
     }
     await svg.save("out.svg");
 }
